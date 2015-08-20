c1fb4320e706cec95f218dd523208042581cdc5a
Anton Temchenko <anton.temchenko@docusafe.pl>
2015-08-20 14:04:53 +0200

    created color randomizer

---
+++ script.js
@@ -0,0 +1,7 @@
+function go(){
+	return Math.round(Math.random() * 255);
+}
+
+$('button').click(function(){
+	$('div').css({ "background-color": "rgb(" + go() + "," + go() + "," + go() + ")" });	
+})
