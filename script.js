7cb7661b01b34003bb41c5ac99b63c4e1592c4f0
Anton Temchenko <anton.temchenko@docusafe.pl>
2015-08-21 13:09:24 +0200

    Added displaying the dec and hex color code

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ $( document ).ready(function() {
 		return Math.round(Math.random() * 255);
 	}
 
+	function componentToHex(c) {
+	    var hex = c.toString(16);
+	    return hex.length == 1 ? "0" + hex : hex;
+	}
+
 	$('button').click(function(){
-		$('div').css({ "background-color": "rgb(" + go() + "," + go() + "," + go() + ")" });	
+		var r = go();
+		var g = go();
+		var b = go();
+		$('div').css({ "background-color": "rgb(" + r + "," + g + "," + b + ")" });	
+		$('.dec').html("rgb(" + r + "," + g + "," + b + ") &nbsp;" +  "#" + componentToHex(r) + componentToHex(g) + componentToHex(b));
 	})
 });
